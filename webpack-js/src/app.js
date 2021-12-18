0d99420893a8cc687efc0c145c737416e6a889cf
ll-bat <gshatakishvili@gmail.com>
2021-12-18 19:41:38 +0400

    Remove extra lines from app.js

--- webpack-js/src/app.js
+++ webpack-js/src/app.js
@@ -1,8 +1,2 @@
-// import $game from "./general/Game";
-// import {GAME_TYPE_RELAX} from "./general/game_types/Constants";
-//
-// $game.init(GAME_TYPE_RELAX);
-// $game.start();
-
 import Menu from "./general/menu/Menu";
 Menu.init()
