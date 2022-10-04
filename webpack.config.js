ab6610c7ca7aaa0e223d531456534ce13232be49
vladimer <gshatakishvili@gmail.com>
2022-10-04 13:11:30 +0400

    Prepare for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require('path')
 
-module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './src/app.js',
-    output: {
-        filename: 'build.js',
-        path: path.join(__dirname, './dist/build.js')
-    },
-    module: {
-        rules: [{
-            test: /\.css?$/,
-            use: ['style-loader', 'css-loader'],
-        }]
-    },
-    watch: true,
+module.exports = env => {
+    return {
+        mode: 'development',
+        devtool: 'source-map',
+        entry: './src/app.js',
+        output: {
+            filename: 'build.js',
+            path: path.join(__dirname, './dist/build.js')
+        },
+        module: {
+            rules: [{
+                test: /\.css?$/,
+                use: ['style-loader', 'css-loader'],
+            }]
+        },
+        watch: env.mode === "development" ? true : false,
+    }
 }
